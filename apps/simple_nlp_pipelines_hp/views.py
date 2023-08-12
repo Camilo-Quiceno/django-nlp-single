839e174228abeae0a65a68ad61575937840ada96
Camilo-Quiceno <camilo.quiceno.q@gmail.com>
2023-08-11 21:22:27 -0500

    First pipeline is working

--- apps/simple_nlp_pipelines_hp/views.py
+++ apps/simple_nlp_pipelines_hp/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render, redirect
 from django.template import RequestContext
 from django.contrib import messages
+from transformers import pipeline
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
 def register_input(request):
+    
+    text_classifier = pipeline(model="distilbert-base-uncased-finetuned-sst-2-english")
+    
     text = request.POST['text_area']
-    messages.success(request, text)
+    
+    output_classifier = text_classifier(text)
+    label_classfier = output_classifier[0]['label']
+    score_classifier = output_classifier[0]['score']
+    
+    output = f'The text is classified as: {label_classfier.capitalize()} with a score of {score_classifier}'
+    messages.success(request, output)
     return redirect('/', RequestContext(request))
