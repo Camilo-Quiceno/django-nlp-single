8518cf216c71b45aae7eb9db5c88e7401c4697f9
Camilo-Quiceno <camilo.quiceno.q@gmail.com>
2023-08-22 21:34:03 -0500

    All the pipelines are working with ideal text input

--- apps/simple_nlp_pipelines_hp/views.py
+++ apps/simple_nlp_pipelines_hp/views.py
@@ -13,23 +13,70 @@ def register_input(request):
     
     match request.session['pipeline_selected']:
         case "1":
-            text = "The input to this task is a corpus of text and the model will output a summary of it based on the expected length mentioned in the parameters. Here, we have kept minimum length as 5 and maximum length as 30."
+            summarizer = pipeline(
+                "summarization", model="t5-base", tokenizer="t5-base", framework="tf"
+            )
+            
+            output_summarizer = summarizer(text, min_length=5, max_length=35)[0]['summary_text']
+            output = f'The text summarize is: {output_summarizer}'
         case "2":
-            text = "In this task, we provide a question and a context. The model will choose the answer from the context based on the highest probability score. It also provides the starting and ending positions of the text."
+            question = text.split(",")[0]
+            context = text.split(",")[1]
+        
+            question_answering = pipeline(model="deepset/roberta-base-squad2")
+            
+            output_question_answering = question_answering(
+                                question=question,
+                                context=context,
+                            )
+            answer_qa = output_question_answering['answer']
+            score_qa = output_question_answering['score']
+            output = f'The answer of the question is: {answer_qa} with a score of: {score_qa}'
         case "3":
-            text = "Named Entity Recognition deals with identifying and classifying the words based on the names of persons, organizations, locations and so on. The input is basically a sentence and the model will determine the named entity along with its category and its corresponding location in the text. "
+            entity_classifier = pipeline(
+                model="dslim/bert-base-NER-uncased", aggregation_strategy="simple"
+            )
+            
+            sentence = text
+            entities = entity_classifier(sentence)
+            output = ""
+            for entity in entities:
+                    entity_group = entity['entity_group']
+                    word = entity['word']
+                    single_output = f'- The entity group of the word {word} is {entity_group}\n'
+                    output += single_output
         case "4":
-            text = "PoS Tagging is useful to classify the text and provide its relevant parts of speech such as whether a word is a noun, pronoun, verb and so on. The model returns PoS tagged words along with their probability scores and respective locations."
+            pos_tagger = pipeline(
+                model="vblagoje/bert-english-uncased-finetuned-pos",
+                aggregation_strategy="simple",
+            )
+            
+            taggings = pos_tagger(text)
+            output = ""
+            for tagging in taggings:
+                    entity_group = tagging['entity_group']
+                    word = tagging['word']
+                    single_output = f'The entity group of the word {word} is {entity_group}\n'
+                    output += single_output
+            
         case "5":
             text_classifier = pipeline(model="distilbert-base-uncased-finetuned-sst-2-english")
+            
             output_classifier = text_classifier(text)
             label_classfier = output_classifier[0]['label']
             score_classifier = output_classifier[0]['score']
             output = f'The text is classified as: {label_classfier.capitalize()} with a score of {score_classifier}'
         case "6":
-            text = "Text will be generated base on your input."
+            text_generator = pipeline(model="gpt2")
+            
+            generated_text = text_generator(text, do_sample=False)[0]['generated_text']
+            output = f'The text generated is: {generated_text}'
         case "7":
-            text = "Here, we will translate the language of text from one language to another. English -> French"
+            en_fr_translator = pipeline("translation_en_to_fr", model='t5-small')
+            
+            output_translator = en_fr_translator(text)
+            text_translated = output_translator[0]['translation_text']
+            output = f'The text translated is: {text_translated}'
         case _:
             text = "Caso predeterminado"
     
